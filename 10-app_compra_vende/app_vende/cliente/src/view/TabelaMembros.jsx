e84a98d37f4b561915fc0165ff43ffcde3fffeec
Emilio <emilio-51@hotmail.com>
2020-11-04 14:20:30 -0300

    Matriculas

--- 10-app_compra_vende/app_vende/cliente/src/view/TabelaMembros.jsx
+++ 10-app_compra_vende/app_vende/cliente/src/view/TabelaMembros.jsx
@@ -8,14 +8,29 @@ const TabelaMembros = () => {
           <tr><th>Nome</th><th>Matrícula</th></tr>
         </thead>
         <tbody>
-            <tr>
-                <td>João Gabriel Trombeta</td>
-                <td>15200598</td>
-            </tr>
+          <tr>
+            <td>Caio de Oliveira Kretzer</td>
+            <td>18250262</td>
+          </tr>
+          <tr>
+            <td>Gabriel Muller</td>
+            <td>16100728</td>
+          </tr>
+          <tr>
+            <td>Gabriel Vitor Garcia do Espirito Santo</td>
+            <td>18150247</td>
+          </tr>
+          <tr>
+            <td>João Gabriel Trombeta</td>
+            <td>15200598</td>
+          </tr>
+          <tr>
+            <td>Marcelo Emilio Vendramin</td>
+            <td>16200653</td>
+          </tr>
         </tbody>
       </table>
     )
 }
-  
+
 export default TabelaMembros
-  
